f7d1ff4430849e0c372c03e826de7a2507944800
Maikel <Lineaclientemiami69@gmail.com>
2026-03-25 23:54:46 -0500

    style: corregir nomenclatura a PascalCase y actualizar estructura de datos

--- prestamos.java
+++ prestamos.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class prestamos {
+public class Prestamos {
 
     String id;
     String isbnLibro;
@@ -8,16 +8,15 @@ public class prestamos {
     String fechaPrestamo;
     boolean activo;
 
-    public prestamos( String id, String isbnLibro, String documentoUsuario){
+    public Prestamos(String id, String isbnLibro, String documentoUsuario) {
         this.id = id;
         this.isbnLibro = isbnLibro;
         this.documentoUsuario = documentoUsuario;
-        this.fechaPrestamo = LocalDate.now() . toString();
+        this.fechaPrestamo = LocalDate.now().toString();
         this.activo = true;
     }
 
-    public void devolucionLibro(){
-     this.activo = false;   
+    public void devolucionLibro() {
+        this.activo = false;
     }
-    
 }
